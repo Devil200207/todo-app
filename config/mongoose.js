f84df8df68d5e6e46d6ed5486cb6f1e99750ccdb
Devil200207 <74138871+Devil200207@users.noreply.github.com>
2022-11-09 21:16:58 +0530

    database url change

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // connect to database
-mongoose.connect('mongodb+srv://chinmay:chinmay.cc@cluster0.ywncej6.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://chinmay:chinmay@cluster0.ywncej6.mongodb.net/?retryWrites=true&w=majority');
 
 // accure the connection to check if it is succesfull
 const db = mongoose.connection;
